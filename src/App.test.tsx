d4c0c7f937353483983e431bc00f975c94d0403f
Mircea Dumitrescu <mirceagab@gmail.com>
2022-10-31 08:57:26 +0100

    initial setup 3

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-
-describe('App renders correctly', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
-  })
-})
