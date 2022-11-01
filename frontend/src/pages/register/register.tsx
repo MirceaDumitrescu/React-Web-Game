e03d798cd36924f97cbb3214e8f1556ed7f61fcc
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2022-11-01 18:10:50 +0200

    Created a single function for input validation
    1. Created. a single function for every validation.
    2. Moved the confirmPass validation functionality in a function.
    
    I will work in storing the registered users inside local storage.

--- frontend/src/pages/register/register.tsx
+++ frontend/src/pages/register/register.tsx
@@ -16,7 +16,25 @@ const RegisterComponent = () => {
     },
   })
 
-  console.log(errors)
+  const passValidation = (val: string) => {
+    if (watch('password') != val) {
+      return 'Your password do not match!'
+    }
+  }
+
+  const errorsRender = (field: any) => {
+    if (field && field.type === 'required') {
+      return <span role='alert'>This is required!</span>
+    } else if (field && field.type === 'minLength') {
+      return <span role='alert'>Password can not be shorter than 8 characters!</span>
+    } else if (field && field.type === 'maxLength') {
+      return <span role='alert'> Password can not be longer then 20 characters!</span>
+    } else if (field && field.type === 'validate') {
+      return <span role='alert'>The passwords do not match!</span>
+    } else {
+      return null
+    }
+  }
 
   return (
     <div className='card'>
@@ -32,25 +50,20 @@ const RegisterComponent = () => {
           <input
             id='name'
             type='text'
-            {...register('name', { required: true, minLength: 3 })}
+            {...register('name', { required: true })}
             className='card__form__group__input'
-          ></input>
-          {errors.name && errors.name.type === 'required' && (
-            <span role='alert'>This is required</span>
-          )}
+          />
+          {errorsRender(errors.name)}
         </div>
         <div className='card__form__group'>
           <label>Email address:</label>
           <input
             id='email'
             type='email'
-            {...register('email', { required: true, minLength: 3 })}
+            {...register('email', { required: true })}
             className='card__form__group__input'
           ></input>
-          {errors.email && errors.email.type === 'required' && (
-            <span role='alert'>This is required</span>
-          )}
-          {errors.email && errors.email.type === 'minLength'}
+          {errorsRender(errors.email)}
         </div>
         <div className='card__form__group'>
           <label>Password:</label>
@@ -60,15 +73,7 @@ const RegisterComponent = () => {
             {...register('password', { required: true, minLength: 8, maxLength: 20 })}
             className='card__form__group__input'
           ></input>
-          {errors.password && errors.password.type === 'required' && (
-            <span role='alert'>This is required</span>
-          )}
-          {errors.password && errors.password.type === 'minLength' && (
-            <span role='alert'>Password can not be shorter than 8 characters!</span>
-          )}
-          {errors.password && errors.password.type === 'maxLength' && (
-            <span role='alert'>Password can not be longer than 20 characters!</span>
-          )}
+          {errorsRender(errors.password)}
         </div>
         <div className='card__form__group'>
           <label htmlFor='confirmPass'>Confirm password:</label>
@@ -79,26 +84,11 @@ const RegisterComponent = () => {
               required: true,
               minLength: 8,
               maxLength: 20,
-              validate: (val: string) => {
-                if (watch('password') != val) {
-                  return 'Your password do not match!'
-                }
-              },
+              validate: passValidation,
             })}
             className='card__form__group__input'
           ></input>
-          {errors.confirmPass && errors.confirmPass.type === 'required' && (
-            <span role='alert'>This is required</span>
-          )}
-          {errors.confirmPass && errors.confirmPass.type === 'minLength' && (
-            <span role='alert'>Password can not be shorter than 8 characters!</span>
-          )}
-          {errors.confirmPass && errors.confirmPass.type === 'maxLength' && (
-            <span role='alert'>Password can not be longer than 20 characters!</span>
-          )}
-          {errors.confirmPass && errors.confirmPass.type === 'validate' && (
-            <span role='alert'>The passwords do not match!</span>
-          )}
+          {errorsRender(errors.confirmPass)}
         </div>
         <button id='btn-submit' className='card__form__btn' type='submit'>
           Register
