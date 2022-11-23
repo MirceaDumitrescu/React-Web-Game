69709d82e7be1503980662611cffe9f9f722d60e
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2022-11-23 19:36:21 +0100

    Made a first issue of how the character creation should look and work.
    At the moment i left some comments inside the component regarding of how it should work.
    
    Maybe we can discuss how  to implement everything?

--- frontend/src/pages/character-creation/chracter-creation.tsx
+++ frontend/src/pages/character-creation/chracter-creation.tsx
@@ -14,24 +14,27 @@ function CharacterCreation() {
         </div>
         <div className='char__input'>
           <label>Character name:</label>
-          <input name='charName' type='text' />
+          <input name='charName' type='text' className='char__input__name' />
         </div>
         <div className='char__stats'>
+          {/* Here the stats will varry depending on the class the user will select */}
           <div className='char__stats__cont'>
             <p className='char__stat'>ATK</p>
             <p className='char_value'>25</p>
           </div>
-          <div className='char_stats_cont'>
+          <div className='char__stats__cont'>
             <p className='char__stat'>DEF</p>
             <p className='char__value'>10</p>
           </div>
-          <div>
-            <p>INT</p>
-            <p>15</p>
+          <div className='char__stats__cont'>
+            <p className='char__stat'>INT</p>
+            <p className='char__value'>15</p>
           </div>
         </div>
+        <button className='char__btn'>Save</button>
       </div>
       <div className='char__preview'>
+        {/* Here the user can preview his character based on the class he selects. */}
         <h3>Your character here!</h3>
       </div>
     </div>
