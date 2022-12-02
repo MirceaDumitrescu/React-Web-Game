df3beea07c06fbdfd301b655f16e78de9b610c11
Mircea Dumitrescu <mirceagab@gmail.com>
2022-12-02 09:51:26 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import router from './routes'
 import './App.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import DataLayerComponent from './features/utils/DataLayer'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
@@ -10,6 +11,18 @@ function App() {
       <DataLayerComponent>
         <RouterProvider router={router} />
       </DataLayerComponent>
+      <ToastContainer
+        position='bottom-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
     </div>
   )
 }
