e247ee07d189b43f058267669b51db2072250b59
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2022-12-02 14:30:45 +0200

    Work in progress..

--- frontend/src/pages/character-creation/chracter-creation.tsx
+++ frontend/src/pages/character-creation/chracter-creation.tsx
@@ -1,10 +1,9 @@
 import './character-creation.scss'
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState, useMemo, useRef } from 'react'
 import classes from './char_class.json'
 
 type CharacterClass = {
-  id: number
-  name: string
+  class: string
   ATK: number
   INT: number
   DEF: number
@@ -13,14 +12,24 @@ type CharacterClass = {
 
 function CharacterCreation() {
   const defaultClass: CharacterClass = useMemo(() => {
-    return classes.find((item: CharacterClass) => item.name === 'Mage')
+    return classes.find((item: CharacterClass) => item.class === 'Mage')
   }, [classes]) as CharacterClass
 
   const [selectedClass, setSelectedClass] = useState(defaultClass)
+  const [characterName, setCharacterName] = useState('')
+  const [charData, setCharData] = useState({})
+
+  // const inputRef: any = useRef(null)
+
+  // useEffect(() => {
+  //   console.log(selectedClass)
+  // }, [selectedClass])
 
   useEffect(() => {
-    console.log(selectedClass)
-  }, [selectedClass])
+    setCharData(() => ({ name: characterName, selectedClass }))
+    console.log('Selected Class', selectedClass)
+    console.log('Char Data', charData)
+  }, [characterName, selectedClass])
 
   interface charData {
     name: string
@@ -36,6 +45,7 @@ function CharacterCreation() {
     try {
       const response = await fetch('http://localhost:5050/api/character/ ')
       const data = await response.json()
+      // console.log(inputRef.current.value)
       console.log(data)
       return data
     } catch (error: any) {
@@ -47,18 +57,27 @@ function CharacterCreation() {
     <div className='char'>
       <div className='char__container'>
         <div>
-          <input className='char__input' type='text' placeholder='Enter your character name...' />
+          <input
+            className='char__input'
+            type='text'
+            value={characterName}
+            onChange={(e) => {
+              setCharacterName(e.target.value)
+            }}
+            // ref={inputRef}
+            placeholder='Enter your character name...'
+          />
         </div>
         <div className='char__config'>
           {classes.map((item: CharacterClass) => (
-            <div key={item.id}>
+            <div key={item.class}>
               <button
                 className='char__btn'
                 onClick={() => {
-                  setSelectedClass(item)
+                  setSelectedClass(() => item)
                 }}
               >
-                {item.name}
+                {item.class}
               </button>
             </div>
           ))}
